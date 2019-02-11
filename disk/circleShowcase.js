b523f8c5100b17af736fec1ebe8ce99c2eee94dd
Chris Till <cpaultill@gmail.com>
2019-02-11 15:53:03 -0500

    Add distance from servant graph.

--- disk/circleShowcase.js
+++ disk/circleShowcase.js
@@ -35,9 +35,14 @@ var graphSVG; //0:Background - 1:Line - 2:Bots - 3:Overlay
 graphDots = [];
 graphPoints = [];
 graphLine = [];
+
+var distanceFromSVG;
+
 var exitFoundLine = null;
 var allExitedLine = null;
 var priorityExitedLine = null;
+
+var distanceFromServant = [];
 
 var b = d3.select('body').append('div');
 
@@ -55,7 +60,7 @@ function MSlide() {
   var mousePos = d3.event.x;
   if (mousePos < unit2Px * (10/25)) {
     mousePos = unit2Px * (10/25);
-} else if (mousePos > (71 / 20) * unit2Px) {
+  } else if (mousePos > (71 / 20) * unit2Px) {
     mousePos = (71 / 20) * unit2Px;
   }
   d3.select(this).attr("x", mousePos);
@@ -143,7 +148,8 @@ class Tourist {
       this.allowance = 0;
     }
     if (this.x == fieldExit[0] && this.y == fieldExit[1] && !this.atExit) {
-        console.log("Robot " + this.number + " exits at " + (Math.floor((100 * time) / fps) / 100));
+
+        console.log("Robot " + this.number + " exits at " + (Math.floor((100 * time) / fps) / 100) + (this.p ? "(Priority)" : ""));
         this.atExit = true;
         /*
         graphSVG.select(".overLay").append("line").attr("x1", graphDots[this.number].attr("cx")).attr("y1", 4 * unit2Px - unit2Px * (10 / 25))
@@ -450,6 +456,8 @@ function Load() {
                .on("mousemove", ChoosExit).on("click", exitChosen);
   graphSVG = b.append("svg").attr("width", unit2Px * 4).attr("height", unit2Px * 4)
                .style("float", "left").style("border", "1px solid black");
+  distanceFromSVG = b.append("svg").attr("width", unit2Px * 4).attr("height", unit2Px * 2)
+               .style("float", "left").style("border", "1px solid black").attr("class", "distanceFrom");
   var classes = ["backGround", "lines", "bots", "overLay"];
   for (var i = 0; i < classes.length; i++) {//Create layers
     fieldSVG.append("svg").attr("width", unit2Px * 4).attr("height", unit2Px * 4)
@@ -473,10 +481,10 @@ function Load() {
     graphPoints.push([]);
     touristNum++;
   }
-  
-  
+
+
   var touristCount = 0;
-  outer: 
+  outer:
   for (var i = 0; i <= 2; i++){
       if (touristCount + 1 == instruBinder.length){
           break outer;
@@ -498,10 +506,10 @@ function Load() {
               break outer;
           }
           touristCount ++;
-                  
+
       }
   }
-  
+
 
   dataBox = fieldSVG.select(".overLay").append("svg").attr("width", 2 * unit2Px).attr("height", unit2Px).attr("visibility", "hidden");
   for (var i = 0; i < tourists.length; i++){
@@ -511,13 +519,16 @@ function Load() {
     LoadAnim();
   }
 
+  console.log(distanceFromServant);
+
+
   time = 0;
   for (var i = 0; i < touristNum; i++) {//Reset for next run through.
     tourists[i].on = 1;
     tourists[i].a = 0;
     tourists[i].x = center[0];
     tourists[i].y = center[1];
-    
+
     var holdA = 'M' + (tourPoints[i][0].x + ',' + (tourPoints[i][0].y));
     var holdG = 'M' + (graphPoints[i][0].x + ',' + (graphPoints[i][0].y));
     /*
@@ -531,9 +542,142 @@ function Load() {
                   .style("stroke", tourColors[i]).style("stroke-width", unit2Px * (1 / 25)).style("stroke-opacity", 0.5).style("fill", "none");
     graphLine[i] = graphSVG.select(".lines").append("path").attr("d", holdG)
                    .style("stroke", tourColors[i]).style("stroke-width", unit2Px * (1 / 25)).style("stroke-opacity", 0.5).style("fill", "none");
-    
+
   }
+
+  for (var i = 0; i < distanceFromServant.length; i++){
+      distanceFromServant[i] = 4 * (1 -  (distanceFromServant[i] / (unit2Px * 4)));
+  }
+
+  console.log(distanceFromServant);
+
+  // 2. Use the margin convention practice
+  var margin = {top: 50, right: 50, bottom: 50, left: 50}
+    , width = (unit2Px * 4) - margin.left - margin.right // Use the window's width
+    , height = (unit2Px * 2) - margin.top - margin.bottom; // Use the window's height
+
+  // The number of datapoints
+  var n = 600;
+
+  // 5. X scale will use the index of our data
+  var xScale = d3.scaleLinear()
+      .domain([0, n-1]) // input
+      .range([0, width]); // output
+
+  // 6. Y scale will use the randomly generate number
+  var yScale = d3.scaleLinear()
+      .domain([0, 2]) // input
+      .range([height, 0]); // output
+
+  // 7. d3's line generator
+  var line = d3.line()
+      .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+      .y(function(d) { return yScale(d.y); }) // set the y values for the line generator
+      .curve(d3.curveMonotoneX) // apply smoothing to the line
+
+  // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+  var dataset = [];
+  for (var i = 0; i < distanceFromServant.length; i++){
+      dataset.push({"y" : distanceFromServant[i]});
+  }
+
+  // 1. Add the SVG to the page and employ #2
+  distanceFromSVG
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // 3. Call the x axis in a group tag
+  distanceFromSVG.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(75," + (height + 25) + ")")
+      .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+
+  // 4. Call the y axis in a group tag
+  distanceFromSVG.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(75, 25)")
+      .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+
+  distanceFromSVG.append("text")
+      .attr("class", "x axis label")
+      .attr("text-align", "center")
+      .attr("x", (2 * unit2Px) - 45)
+      .attr("y", height + 75)
+      .text("Time (Frames)");
+
+  distanceFromSVG.append("text")
+      .attr("class", "y axis label")
+      .attr("text-align", "center")
+      .attr("x", -height)
+      .attr("y", 25)
+      .attr("transform", "rotate(-90)")
+      .text("Distance From Servant (Units)");
+
+  distanceFromSVG.append("text")
+      .attr("class", "distance from title")
+      .attr("text-align", "center")
+      .attr("x", (unit2Px))
+      .attr("y", 30)
+      .text("Priority Bot 1 Distance From Servant (No-Exit Simulation)")
+
+  // 9. Append the path, bind the data, and call the line generator
+  distanceFromSVG.append("path")
+      .datum(dataset) // 10. Binds data to the line
+      .attr("class", "line") // Assign a class for styling
+      .attr("transform", "translate(75, 25)")
+      .attr("d", line); // 11. Calls the line generator
+
+  var focus = distanceFromSVG.append("g")
+      .attr("transform", "translate(0, 25)")
+      .attr("class", "focus")
+      .style("display", "none");
+
+  focus.append("circle")
+      .attr("r", 4.5);
+
+  focus.append("text")
+      .attr("x", 9)
+      .attr("dy", ".35em");
+
+  distanceFromSVG.append("rect")
+      .attr("class", "overlay")
+      .attr("width", distanceFromSVG.attr("width"))
+      .attr("height", distanceFromSVG.attr("height"))
+      .attr("transform", "translate(75, 25)")
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+  var formatValue = d3.format(",.3f");
+
+  function mousemove() {
+    var coords = d3.mouse(this);
+    //console.log(xScale.invert(coords[0]));
+    var x0 = xScale.invert(coords[0]),
+        i = Math.floor(x0)
+        d0 = dataset[i - 1],
+        d1 = dataset[i],
+        d = x0 - d0 > d1 - x0 ? d1 : d0;
+    console.log((height - (d1 * unit2Px * 2)));
+    if (i < 1 || i > 599){
+        focus.attr("display", "none");
+    }
+    else{
+        focus.attr("display", "null");
+        focus.attr("transform", "translate(" + (coords[0] + 75) + "," + (yScale(d.y) + 25) + ")");
+        focus.select("text").text(i + ", " + formatValue(dataset[i].y));
+    }
+
+  }
+
+  if (distanceFromServant.length < 600){
+      distanceFromSVG.attr("display", "none");
+  }
+
 }
+
+
+
 
 
 function LoadField() {
@@ -620,20 +764,20 @@ function LoadGraph() {
             .attr("y", unit2Px * (90 / 25) - i * unit2Px * (20 / 25))
             .style("font-size", unit2Px * (4 / 25)).style("text-anchor", "middle").text(i + 'r');
   }
-  
+
   for (var i = 0; i < 11; i++) {//Create x-axis labels.
     graphSVG.select(".backGround").append("text").attr("x", unit2Px * (10 / 25) + i * unit2Px * (8 / 25))
             .attr("y", unit2Px * (94 / 25))
             .style("font-size", unit2Px * (4 / 25)).style("text-anchor", "middle").text(i)
             .attr("class", "graphnum");
   }
-  
+
   graphSVG.select(".backGround").append("text").attr("x", unit2Px * (5 / 25)).attr("y", unit2Px * (70 / 25)).attr("text-anchor", "middle")
           .attr("transform", "rotate(-90," + unit2Px * (5 / 25) + ',' + unit2Px * (70 / 25) + ')')
           .style("font-size", unit2Px * (5 / 25)).style("fill", "#000000").text("Distance from Exit");
   graphSVG.select(".backGround").append("text").attr("x", unit2Px * 2).attr("y", unit2Px * (395 / 100)).attr("text-anchor", "middle")
           .style("font-size", unit2Px * (5 / 25)).style("fill", "#000000").text("Time");
-  
+
 }
 
 //Reset Animation.
@@ -666,6 +810,11 @@ function Reset() {
     touristNum = 0;
     tourColors = [];
 
+    //distanceFromSVG.selectAll('g').remove();
+    distanceFromSVG.remove();
+
+    distanceFromServant = [];
+
     fieldSVG.selectAll('svg').remove();
     fieldSVG.remove();
     tourists = [];
@@ -679,6 +828,8 @@ function Reset() {
     graphLine = [];
     exitFoundLine = null;
     allExitedLine = null;
+
+
 
     Start();
 
@@ -731,6 +882,9 @@ function LoadPoints(i) {
   var dista = unit2Px * 4 - (Math.sqrt(Math.pow(fieldExit[0] - tourists[i].x, 2) + Math.pow(fieldExit[1] - tourists[i].y, 2)) * (20 / 25));
   tourPoints[i].push({x:tourists[i].x, y:tourists[i].y});
   graphPoints[i].push({x:(unit2Px * (10 / 25) + (time / timeMax) * (80 / 25) * unit2Px), y:(dista - unit2Px * (10 / 25))});
+  if (i==2){
+      distanceFromServant.push(unit2Px * 4 - (Math.sqrt(Math.pow(tourPoints[2][tourPoints[2].length-1].x - this.tourPoints[1][tourPoints[1].length-1].x, 2) + Math.pow(tourPoints[2][tourPoints[2].length-1].y - tourPoints[1][tourPoints[1].length-1].y, 2)) * (20 / 25)));
+  }
 }
 
 //Control times, and update bots.
@@ -739,16 +893,17 @@ function AlterAnim() {
     time = timeMax;
     clearInterval(projector);
     projector = setInterval(PlayAnim, 1000 / fps);
-    
+
     for (var i = 0; i < 11; i++) {//Create x-axis labels.
       graphSVG.selectAll(".graphnum").style("display", "none");
     }
-    
+
     for (i = 0; i < touristNum; i++){
         AlterLines(i);
-        
+
+
     }
-    
+
     for (var i = 0; i <= Math.floor((100 * time) / fps) / 100; i++) {//Create x-axis labels.
       graphSVG.select(".backGround").append("text").attr("x", (10/25 + (i / (Math.floor((100 * time) / fps) / 100)) * 80/25) * unit2Px)//unit2Px * (10 / 25) + ((Math.floor(timeMax) - i) * 80/25) * unit2Px)
               .attr("y", unit2Px * (94 / 25))
@@ -789,11 +944,12 @@ function AlterAnim() {
         }
       }
     }
+
     if (exitAlert) {
       if (exitFoundLine == null) {
         var holdX = (unit2Px * (10 / 25) + ((time + exitAllow) / timeMax) * (80 / 25) * unit2Px);
         exitFoundLine = 1;
-                            
+
                         /*
                         graphSVG.select(".overLay").append("line").attr("x1", holdX).attr("y1", 4 * unit2Px - unit2Px * (10 / 25))
                         .attr("x2", holdX).attr("y2", 2 * unit2Px).style("stroke", "#000000").style("stroke-width", (1 / 100) * unit2Px)
@@ -847,7 +1003,7 @@ function AlterLines(i) {
   var dista = unit2Px * 4 - Math.abs((Math.sqrt(Math.pow(fieldExit[0] - tourists[i].x, 2) + Math.pow(fieldExit[1] - tourists[i].y, 2)) * (20 / 25)));
   tourPoints[i][time] = {x:tourists[i].x, y:tourists[i].y};
   tourLine[i].remove();
-  
+
   var holdA = 'M' + (tourPoints[i][0].x + ',' + (tourPoints[i][0].y));
   var holdG = 'M' + (10/25) * unit2Px + ',' + (graphPoints[i][0].y);
   for (var j = 1; j < time; j++) {
@@ -855,7 +1011,7 @@ function AlterLines(i) {
     holdG += 'L' + ((10/25) + ((graphPoints[i][j].x / timeMax) * (63/20))) * unit2Px  + ',' + (graphPoints[i][j].y);
     //((10/25) + (graphPoints[i][j].x / timeMax) * (63/20)) * unit2Px)
   }
-  
+
   tourLine[i] = fieldSVG.select(".lines").append("path").attr("d", holdA)
                 .style("stroke", tourColors[i]).style("stroke-width", unit2Px * (1 / 25)).style("stroke-opacity", 0.5).style("fill", "none");
   graphPoints[i][time] = {x:time, y:(dista - unit2Px * (10 / 25))};
@@ -876,13 +1032,13 @@ function AllAtExit() {
                             /*graphSVG.select(".overLay").append("line").attr("x1", holdP).attr("y1", 4 * unit2Px - unit2Px * (10 / 25))
                           .attr("x2", holdP).attr("y2", 1.75 * unit2Px).style("stroke", "#000000").style("stroke-width", (1 / 100) * unit2Px)
                           .style("stroke-opacity", 0.5);*/
-        
-        timeMax = time; //to account for the extra bot getting to its next point 
+
+        timeMax = time; //to account for the extra bot getting to its next point
     }
   }
   if (!allExitedLine){
     var holdX = (unit2Px * (10 / 25) + ((time + exitAllow) / timeMax) * (80 / 25) * unit2Px);
-    
+
     allExitedLine = 1;
                     /*graphSVG.select(".overLay").append("line").attr("x1", holdX).attr("y1", 4 * unit2Px - unit2Px * (10 / 25))
                   .attr("x2", holdX).attr("y2", 2 * unit2Px).style("stroke", "#000000").style("stroke-width", (1 / 100) * unit2Px)
@@ -975,25 +1131,45 @@ function showAlgorithmDesc(s, w){
             color = "#efe";
             instruBinder = [
                                  [["GoToExit", [null, true, "#fe447d"]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left", 114]], ["GoToWallAtAngle", [347]], ["FollowWall", ["right", 53]], ["Wait", [null]]],
-                                 [["InterceptNonBeliever", [null, false, "#5cd05b"]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right"]]]
+                                 [["InterceptNonBeliever", [0, false, "#5cd05b"]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right"]]]
                                ];
             algorithmName = "Algorithm Priority 1 ";
             break;
         case 'Q2':
             color = "#efe";
             instruBinder = [
-                                  [["GoToExit", [null, true, "#fe447d"]], ["GoToWallAtAngle", [160]], ["FollowWall", ["left", 20]], ["GoToPoint", [center[0] + 30, center[1] + 30]], ["GoToWallAtAngle", [320]], ["Wait", [null]]],
-                                  [["InterceptNonBeliever", [null, false, "#5cd05b"]], ["GoToWallAtAngle", [160]], ["FollowWall", ["right"]]],
-                                  [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left"]]]
+                                  [["GoToExit", [null, true, "#fe447d"]], ["GoToWallAtAngle", [144]], ["FollowWall", ["left", 36]], ["GoToPoint", [center[0] + (unit2Px * 0.65), center[1] + 30]], ["GoToWallAtAngle", [345]], ["FollowWall", ["left"]]],
+                                  [["InterceptNonBeliever", [0, false, "#5cd05b"]], ["GoToWallAtAngle", [160]], ["FollowWall", ["right"]]],
+                                  [["InterceptNonBeliever", [0, false]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left"]]]
             ];
             algorithmName = "Algorithm Priority 2 ";
             break;
         case 'Q2S1':
             color = "#eee";
             instruBinder = [
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [53]], ["FollowWall", ["left"]]],
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [0]], ["FollowWall", ["left"]]],
+                [["InterceptNonBeliever", [[0, 1], false]], ["GoToWallAtAngle", [0]], ["FollowWall", ["right"]]]
+                /*
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [0]], ["FollowWall", ["left"]]],
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [240]], ["FollowWall", ["left"]]],
+                [["InterceptNonBeliever", [[0, 1], false]], ["GoToWallAtAngle", [240]], ["FollowWall", ["right"]]]
+                */
+                /*
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left"]]],
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right", 36.5]], ["GoToWallAtAngle", [350]], ["Wait", [null]]],
+                [["InterceptNonBeliever", [[0, 1], false]], ["GoToWallAtAngle", [143.5]], ["FollowWall", ["right"]]]
+                */
+                /*
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left"]]],
+                [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right", 45]], ["GoToWallAtAngle", [340]], ["FollowWall", ["left"]]],
+                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [135]], ["FollowWall", ["right"]]]
+                */
+                /*
                 [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left"]]],
                 [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right", 45]], ["GoToPoint", [center[0] + (unit2Px * 0.65), center[1]]], ["GoToWallAtAngle", [337.5]], ["FollowWall", ["left"]]],
                 [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [135]], ["FollowWall", ["right"]]]
+                */
                 /*
                 [["GoToExit", [null]], ["GoToWallAtAngle", [180]], ["FollowWall", ["left", 65]], ["GoToPoint", [center[0], center[1]]], ["GoToWallAtAngle", [245]], ["FollowWall", ["left"]]],
                 [["GoToExit", [null]], ["GoToWallAtAngle", [180]], ["FollowWall", ["right", 65]], ["GoToPoint", [center[0], center[1]]], ["GoToWallAtAngle", [115]], ["FollowWall", ["right"]]],
@@ -1015,7 +1191,7 @@ function showAlgorithmDesc(s, w){
             instruBinder = [
                 [["GoToExit", [null, true]], ["GoToWallAtAngle", [180]] ,["FollowWall", ["right"]]],
                 [["GoToExit", [null, true]], ["GoToWallAtAngle", [0]], ["FollowWall", ["right"]]],
-                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", 180], ["FollowWall", ["left"]]]
+                [["InterceptNonBeliever", [[0,1], false]], ["GoToWallAtAngle", 180], ["FollowWall", ["left"]]]
             ];
             algorithmName = "2 Priority + 1 Servant (2) ";
             break;
@@ -1024,10 +1200,10 @@ function showAlgorithmDesc(s, w){
             color = "#eee";
             instruBinder = [
                 [["GoToExit", [null, true]], ["Wait", [(1 + (Math.PI / 2))]], ["GoToWallAtAngle", [180]]],
-                [["InterceptNonBeliever", [0, false]], ["GoToWallAtAngle", [0]], ["FollowWall", ["left", 75]], ["Wait", [null]]],
-                [["InterceptNonBeliever", [0, false]], ["GoToWallAtAngle", [0]], ["FollowWall", ["right", 75]], ["Wait", [null]]],
-                [["InterceptNonBeliever", [0, false]], ["GoToWallAtAngle", [75]], ["FollowWall", ["left"]]],
-                [["InterceptNonBeliever", [0, false]], ["GoToWallAtAngle", [285]], ["FollowWall", ["right"]]]
+                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [0]], ["FollowWall", ["left", 75]], ["Wait", [null]]],
+                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [0]], ["FollowWall", ["right", 75]], ["Wait", [null]]],
+                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [75]], ["FollowWall", ["left"]]],
+                [["InterceptNonBeliever", [null, false]], ["GoToWallAtAngle", [285]], ["FollowWall", ["right"]]]
             ];
             algorithmName = "1 Priority + 4 Servants ";
             break;
