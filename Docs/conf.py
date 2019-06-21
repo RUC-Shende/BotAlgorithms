d75e9e0775f8de1b23a3819a1d651274c047e318
Chris Till <cpaultill@gmail.com>
2019-06-21 14:16:20 -0400

    Update commit version.

--- Docs/conf.py
+++ Docs/conf.py
@@ -26,7 +26,7 @@ author = 'Chris Till, David Bushta'
 # The short X.Y version
 version = '2.0'
 # The full version, including alpha/beta/rc tags
-release = '2.0.2 (154)'
+release = '2.0.2 (171)'
 # Version [major].[minor].[release] (commit)
 
 # -- General configuration ---------------------------------------------------
